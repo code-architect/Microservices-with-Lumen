052f28c1420c2cde849fb69bb940e90013f29a9a
code-architect <codearchitectdeveloper@gmail.com>
2019-01-03 02:37:30 +0530

    created an isolated new structure with lumen, edited basic requirements and created migrations for books  table

---
+++ LumenBookApi/database/migrations/2019_01_02_202525_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->float('price')->unsigned();
+            $table->integer('author_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
