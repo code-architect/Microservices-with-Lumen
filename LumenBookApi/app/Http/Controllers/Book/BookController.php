4e7a76c927787776b0069f35375d946e6fe0d940
code-architect <codearchitectdeveloper@gmail.com>
2019-01-03 05:05:11 +0530

    created the full CURD operation in BookController

--- LumenBookApi/app/Http/Controllers/Book/BookController.php
+++ LumenBookApi/app/Http/Controllers/Book/BookController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Book;
 use App\Http\Controllers\Controller;
 use App\Models\Book;
 use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 
 class BookController extends Controller
@@ -30,21 +32,31 @@ class BookController extends Controller
     }
 
 
-
-    public function store()
+    /**
+     * Store a single book information
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
     {
         $rules = [
-
+            'title'         =>  'required|max:255',
+            'description'   =>  'required',
+            'price'         =>  'required|min:1',
+            'author_id'     =>  'required|min:1',
         ];
+        $this->validate($request, $rules);
 
+        $book = Book::create($request->all());
 
+        return $this->successResponse($book, Response::HTTP_CREATED);
     }
 
 
     /**
-     * Show a specific book
-     * @param Book $book
-     * @return Response
+     * Storing a single book data
+     * @param $book
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($book)
     {
@@ -54,7 +66,6 @@ class BookController extends Controller
 
 
     /**
-     * Update book information
      * @param Request $request
      * @param $book
      * @return \Illuminate\Http\JsonResponse
@@ -62,15 +73,16 @@ class BookController extends Controller
     public function update(Request $request, $book)
     {
         $rules = [
-            'name'      => 'max:254',
-            'gender'    => 'max:20|in:male,female',
-            'country'   => 'max:254',
+            'title'         =>  'max:255',
+            'description'   =>  'min:1',
+            'price'         =>  'min:1',
+            'author_id'     =>  'min:1',
         ];
         $this->validate($request, $rules);
         $book = Book::findOrFail($book);
         $book->fill($request->all());
         if($book->isClean()){
-            return $this->errorResponse("Atleast one value must change", Response::HTTP_UNPROCESSABLE_ENTITY);
+            return $this->errorResponse("At least one value must change", Response::HTTP_UNPROCESSABLE_ENTITY);
         }
         $book->save();
         return $this->successResponse($book);
