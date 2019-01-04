1dfe1bf2b9324bfb7e32e22b748a83ef69c4ac28
code-architect <codearchitectdeveloper@gmail.com>
2019-01-05 03:09:08 +0530

    created the book controller CURD and BookService

--- LumenApiGateway/app/Http/Controllers/Book/BookController.php
+++ LumenApiGateway/app/Http/Controllers/Book/BookController.php
@@ -13,7 +13,7 @@ class BookController extends Controller
     use ApiResponser;
 
     /**
-     * The service to consume the authors micro-service
+     * The service to consume the books micro-service
      * @var BookService
      */
     public $bookService;
@@ -24,36 +24,58 @@ class BookController extends Controller
     }
 
 
+    /**
+     * Get Book data
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
-
+        return $this->successResponse($this->bookService->obtainBooks());
     }
 
 
-
+    /**
+     * Save an book data
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
-
+        return $this->successResponse($this->bookService->createBook($request->all()));
     }
 
 
-
+    /**
+     * Show a single book details
+     * @param $book
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show($book)
     {
-
+        return $this->successResponse($this->bookService->obtainBook($book));
     }
 
 
-
+    /**
+     * Update a single book data
+     * @param Request $request
+     * @param $book
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, $book)
     {
-
+        return $this->successResponse($this->bookService->editBook($request->all(),$book));
     }
 
 
-
+    /**
+     * Delete a single book details
+     * @param $book
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy($book)
     {
-
+        return $this->successResponse($this->bookService->deleteBook($book));
     }
+
 }
--- LumenApiGateway/app/Services/BookService.php
+++ LumenApiGateway/app/Services/BookService.php
@@ -18,4 +18,30 @@ class BookService
     {
         $this->baseUri = config('services.books.base_uri');
     }
+
+
+    public function obtainBooks()
+    {
+        return $this->performRequest('GET', '/books');
+    }
+
+    public function createBook($data)
+    {
+        return $this->performRequest('POST', '/books', $data);
+    }
+
+    public function obtainBook($book)
+    {
+        return $this->performRequest('GET', "/books/{$book}");
+    }
+
+    public function editBook($data, $book)
+    {
+        return $this->performRequest('PUT', "/books/{$book}", $data);
+    }
+
+    public function deleteBook($book)
+    {
+        return $this->performRequest('DELETE', "/books/{$book}");
+    }
 }
